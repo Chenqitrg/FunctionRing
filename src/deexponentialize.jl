58d3a7681a1a954c393d09e6be5bd53c809b7a5c
Chenqi Meng <mcqgravity@gmail.com>
2025-12-22 22:00:19 +0900

    Corrected some bug in deexponentialize

--- src/deexponentialize.jl
+++ src/deexponentialize.jl
@@ -6,18 +6,19 @@ function Lie_action(ϵ::Vector{E}, f::HoloPoly{R,E}) where {R,E} # TODO: test
     return f_temp
 end
 
-function deexponentialize(f::HoloPoly{Int,E}, trunc::Int) where {R,E} # TODO: test
+function deexponentialize(f::HoloPoly{Int,E}, trunc::Int) where {E} # TODO: test
     ϵ = [-f[2]]
-    poly = [HoloPoly{R,E}([2], [f[2]])]
+    poly = [HoloPoly{Int,E}([2], [f[2]])]
     for n in 2:trunc
-        poly_last = copy(poly[end])
+        @show n, ϵ
+        poly_last = poly[end]
         for (i, p) in enumerate(poly)
-            poly[i] = Lie_action(ϵ[1:i], p) / (n - i + 1)
+            poly[i] = 1 / E(n - i + 1) * Lie_action(ϵ[1:i], p)
         end
         p_tot_coeff_np1 = sum(poly)[n+1]
         ϵn = p_tot_coeff_np1 - f[n+1]
         push!(ϵ, ϵn)
-        push!(poly, poly_last + HoloPoly{R,E}([n+1], [-ϵn]))
+        push!(poly, poly_last + HoloPoly{Int,E}([n + 1], [-ϵn]))
     end
     return ϵ, poly
 end

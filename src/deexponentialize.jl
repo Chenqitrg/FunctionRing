2b9a6b1ebcdf6bb1a28649314693f5245350bf09
Chenqi Meng <mcqgravity@gmail.com>
2025-12-23 22:49:25 +0900

    Added the exp_Lie_action to test the result easily

--- src/deexponentialize.jl
+++ src/deexponentialize.jl
@@ -1,16 +1,36 @@
-function Lie_action(ϵ::Vector{E}, f::HoloPoly{R,E}) where {R,E} # TODO: test
+function Lie_action(ϵ::Vector{E}, f::HoloPoly{R,E}; trunc=O{R}(Inf)) where {R,E} # TODO: test
     f_temp = zero(R, E)
     for (n, ϵn) in enumerate(ϵ)
         f_temp = f_temp + ϵn * Witt_action(n, f)
     end
-    return f_temp
+    return f_temp + trunc * f
 end
 
-function deexponentialize(f::HoloPoly{Int,E}, trunc::Int) where {E} # TODO: test
+function exp_Lie_action(ϵ::Vector{E}, f::HoloPoly{R,E}; trunc=O{R}(Inf)) where {R,E}
+    f_temp_add = f
+    f_temp_action = f
+    n = 1
+    while n < trunc
+        f_temp_action = 1/n * Lie_action(ϵ, f_temp_action; trunc)
+        f_temp_add = f_temp_add + f_temp_action
+        n += 1
+    end
+    return f_temp_add
+end
+
+function deexponentialize(f::HoloPoly{Int,E}, trunc::O{Int}) where {E} # TODO: test
+    if !(f[1] ≈ 1)
+        throw(ArgumentError("Wrong polynomial"))
+    end
+    if trunc == O{Int}(Inf) || f.trunc == O{Int}(Inf)
+        throw(ArgumentError("The truncation needs to be non-infinity"))
+    end
+
     ϵ = [-f[2]]
+    println("The coefficient of the 1-th generator has been calculated")
     poly = [HoloPoly{Int,E}([2], [f[2]])]
-    for n in 2:trunc
-        @show n, ϵ
+    real_trunc = min(floor(Int, trunc.trunc), floor(Int, f.trunc.trunc - 1))
+    for n in 2:real_trunc
         poly_last = poly[end]
         for (i, p) in enumerate(poly)
             poly[i] = 1 / E(n - i + 1) * Lie_action(ϵ[1:i], p)
@@ -19,6 +39,8 @@ function deexponentialize(f::HoloPoly{Int,E}, trunc::Int) where {E} # TODO: test
         ϵn = p_tot_coeff_np1 - f[n+1]
         push!(ϵ, ϵn)
         push!(poly, poly_last + HoloPoly{Int,E}([n + 1], [-ϵn]))
+        println("The coefficient of the $n-th generator has been calculated")
     end
-    return ϵ, poly
+    println("Final truncation: $real_trunc")
+    return ϵ, O{Int}(real_trunc)
 end

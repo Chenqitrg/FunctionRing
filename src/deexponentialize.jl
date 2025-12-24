01ec28c531ec723518224e3eec4ff4f23ea6f34b
Chenqi Meng <mcqgravity@gmail.com>
2025-12-24 23:51:47 +0900

    Added more tests and fixed some bugs

--- src/deexponentialize.jl
+++ src/deexponentialize.jl
@@ -10,12 +10,16 @@ function exp_Lie_action(ϵ::Vector{E}, f::HoloPoly{R,E}; trunc=O{R}(Inf)) where 
     f_temp_add = f
     f_temp_action = f
     n = 1
-    while n < trunc
-        f_temp_action = 1/n * Lie_action(ϵ, f_temp_action; trunc)
-        f_temp_add = f_temp_add + f_temp_action
-        n += 1
+    if trunc == O{Int}(1)
+        return f + trunc * f
+    else
+        while n < trunc
+            f_temp_action = 1 / n * Lie_action(ϵ, f_temp_action; trunc)
+            f_temp_add = f_temp_add + f_temp_action
+            n += 1
+        end
+        return f_temp_add
     end
-    return f_temp_add
 end
 
 function deexponentialize(f::HoloPoly{Int,E}, trunc::O{Int}) where {E} # TODO: test
@@ -26,21 +30,30 @@ function deexponentialize(f::HoloPoly{Int,E}, trunc::O{Int}) where {E} # TODO: t
         throw(ArgumentError("The truncation needs to be non-infinity"))
     end
 
-    ϵ = [-f[2]]
-    println("The coefficient of the 1-th generator has been calculated")
-    poly = [HoloPoly{Int,E}([2], [f[2]])]
-    real_trunc = min(floor(Int, trunc.trunc), floor(Int, f.trunc.trunc - 1))
-    for n in 2:real_trunc
-        poly_last = poly[end]
-        for (i, p) in enumerate(poly)
-            poly[i] = 1 / E(n - i + 1) * Lie_action(ϵ[1:i], p)
+    real_trunc = min(floor(Int, trunc.trunc), floor(Int, f.trunc.trunc - 1)) # A truncation of derivation series, ϵ₁l₁ + ϵ₂l₂ + ... + ϵ_realtrunc-1*l_real_trunc-1 + O(real_trunc)
+
+    if real_trunc == 1
+        return E[], O{Int}(1)
+    else
+        ϵ = [-f[2]]
+        println("The coefficient of the 1-th generator has been calculated")
+        poly = [HoloPoly{Int,E}([2], [f[2]])]
+        if real_trunc == 2
+            return ϵ, O{Int}(2)
+        else
+            for n in 2:real_trunc-1
+                poly_last = poly[end]
+                for (i, p) in enumerate(poly)
+                    poly[i] = 1 / E(n - i + 1) * Lie_action(ϵ[1:i], p)
+                end
+                p_tot_coeff_np1 = sum(poly)[n+1]
+                ϵn = p_tot_coeff_np1 - f[n+1]
+                push!(ϵ, ϵn)
+                push!(poly, poly_last + HoloPoly{Int,E}([n + 1], [-ϵn]))
+                println("The coefficient of the $n-th generator has been calculated")
+            end
+            println("Final truncation: $real_trunc")
+            return ϵ, O{Int}(real_trunc)
         end
-        p_tot_coeff_np1 = sum(poly)[n+1]
-        ϵn = p_tot_coeff_np1 - f[n+1]
-        push!(ϵ, ϵn)
-        push!(poly, poly_last + HoloPoly{Int,E}([n + 1], [-ϵn]))
-        println("The coefficient of the $n-th generator has been calculated")
     end
-    println("Final truncation: $real_trunc")
-    return ϵ, O{Int}(real_trunc)
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -349,13 +349,18 @@ println("----------------")
 fs = [
     HoloPoly{Int,Float64}([1, 2, 3, 4, 5, 6, 7], [1.0, 4, 6, 4, 1, 0.5, 5.5], O{Int}(8)),
     HoloPoly{Int,Float64}([1, 2, 3, 4, 5, 6, 7], [1.0, 4, 6, 4, 1, 2, 3], O{Int}(8)),
+    HoloPoly{Int,Float64}([1, 2, 3, 4], [1.0, 4, 6, 4], O{Int}(6)),
 ]
 
 @testset "Consistency of exponentialization and deexponentialization" begin
     for f in fs
-        coeff, trunc = deexponentialize(f, O{Int}(10))
-        f_reconst = exp_Lie_action(coeff, HoloPoly{Int,Float64}([1], [1.0]); trunc = trunc)
-        @test f == f_reconst
+        for truncdim in 1:10
+            println("Designed truncation dimension of Lie generator: $truncdim")
+            coeff, trunc = deexponentialize(f, O{Int}(truncdim))
+            println("The calculated Witt algebra generator coefficient: $coeff")
+            f_reconst = exp_Lie_action(coeff, HoloPoly{Int,Float64}([1], [1.0]); trunc=trunc)
+            @test f + O{Int}(truncdim + 1) == f_reconst
+        end
     end
 end
 # coeff = deexponentialize(HoloPoly{Int,Float64}(Vector{Int}(1:10), ones(Float64, 10), O{Int}(11)), 9)
